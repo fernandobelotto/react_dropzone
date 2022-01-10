055f5887984095ce082efcd034436a758e727c87
fernandobosco <fbosco@dlocal.com>
2022-01-10 20:22:33 -0300

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -8,31 +8,50 @@ import {
   Code,
   Grid,
   theme,
+  Container,
+  Heading,
+  UnorderedList,
+  ListItem,
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+import { useDropzone } from 'react-dropzone'
+
+export const App = () => {
+
+  return (
+    <ChakraProvider theme={theme}>
+      <ColorModeSwitcher />
+      <Container>
+        <Heading>React Dropzone</Heading>
+        <Basic />
+      </Container>
+    </ChakraProvider>
+  )
+}
+
+function Basic(props: any) {
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+
+  const files = acceptedFiles.map((file: any) => (
+    <li key={file?.path}>
+      {file?.path} - {file.size} bytes
+    </li>
+  ));
+
+  return (
+    <section className="container">
+      <Box p='10' border='1px solid' borderColor='gray.100' {...getRootProps({ className: 'dropzone' })}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </Box>
+      <aside>
+        <Heading fontSize={'2xl'}>Files</Heading>
+
+        <UnorderedList>
+          <ListItem>{files}</ListItem>
+        </UnorderedList>
+      </aside>
+    </section>
+  );
+}
